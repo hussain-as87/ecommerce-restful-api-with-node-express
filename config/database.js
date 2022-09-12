f21e376bc67b114dd2a399604a21132144f87212
hussein sim <adda7mad@gmail.com>
2022-09-12 06:13:20 +0300

    fixed-product-and-subcategory-validation-update

--- config/database.js
+++ config/database.js
@@ -3,7 +3,7 @@ import "colors";
 export const db_connection = () => {
   mongoose.connect(process.env.DB_URL).then((conn) => {
     console.log(
-      `Database Connection Success : ${conn.connection.host}`.white.bgGreen
+      `Database Connection Successfully : ${conn.connection.host}`.green
     );
   });
   /*     .catch((err) => {
