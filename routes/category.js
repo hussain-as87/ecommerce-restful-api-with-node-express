d03676b0e3444213a1664bff9e4140899b2e3059
Hussain ASelmia <hussain87nyn@gmail.com>
2022-12-20 18:23:21 +0200

    edit-category-validtion

--- routes/category.js
+++ routes/category.js
@@ -1,28 +1,29 @@
 import express from "express";
-import { validationparmsRules } from "../middlewares/ValidatorMiddleware.js";
+import {validationparmsRules} from "../middlewares/ValidatorMiddleware.js";
 import {
-  create,
-  destroy,
-  index,
-  resizeImage,
-  show,
-  update,
-  uploadImage,
+    create,
+    destroy,
+    index,
+    resizeImage,
+    show,
+    update,
+    uploadImage,
 } from "../services/CategoryService.js";
- import {
-  ValidationbodyRulesForCreate,
-  ValidationbodyRulesForUpdate,
+import {
+    ValidationbodyRulesForCreate,
+    ValidationbodyRulesForUpdate,
 } from "../utils/validations/CategoryValidation.js";
 import {permissions} from "../services/AuthService.js"
 import subcategoriesRoute from "./subCategory.js";
+
 const router = express.Router();
 
 //nested route
 router.use("/:categoryId/subcategories", subcategoriesRoute);
 router.get("/", index);
 router.get("/:id", validationparmsRules("id"), show);
-router.post("/",permissions('admin','maneger'), [uploadImage,resizeImage], ValidationbodyRulesForCreate,  create);
-router.put("/:id",permissions('admin','maneger'), [validationparmsRules("id"),uploadImage,resizeImage],  ValidationbodyRulesForUpdate,  update);
-router.delete("/:id", permissions('admin'),validationparmsRules("id"), destroy);
+router.post("/", permissions('admin', 'maneger'), [uploadImage, resizeImage], ValidationbodyRulesForCreate, create);
+router.put("/:id", permissions('admin', 'maneger'), [validationparmsRules("id"), uploadImage, resizeImage], ValidationbodyRulesForUpdate, update);
+router.delete("/:id", permissions('admin'), validationparmsRules("id"), destroy);
 
 export default router;
