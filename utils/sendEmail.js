f15ede188de4c1fd3eae65a5e0730553ef0b45c5
hussain <hussain87nyn@gmail.com>
2023-06-20 08:52:57 +0300

    fixed the sending email handler,the forget password, verfication code handler, and the login and signup handlers

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,36 +1,31 @@
 import nodemailer from 'nodemailer';
-import smtpTransport from 'nodemailer-smtp-transport'
 
-// Nodemailer
 const sendEmail = async (options) => {
-    // 1) Create transporter ( service that will send email like "gmail","Mailgun", "mialtrap", sendGrid)
-    const transporter = nodemailer.createTransport(smtpTransport({
-        service: process.env.EMAIL_SERVICE,
-        port: process.env.EMAIL_PORT, // if secure false port = 587, if true port= 465
-        secure: true,
-        auth: {
-            user: process.env.EMAIL_USER,
-            pass: process.env.EMAIL_PASSWORD,
-        },
-    }));
+  const transporter = nodemailer.createTransport({
+    service: process.env.EMAIL_SERVICE,
+    host: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT,
+    secure: true,
+    auth: {
+      user: process.env.EMAIL_USERNAME,
+      pass: process.env.EMAIL_PASSWORD,
+    },
+  });
 
-    // 2) Define email options (like from, to, subject, email content)
-    const mailOpts = {
-        from: 'adda77mad@gmail.com',
-        to: options.email,
-        subject: options.subject,
-        text: options.message,
-    };
+  const mailOpts = {
+    from: `E-Shop HAS<${process.env.EMAIL_USERNAME}>`,
+    to: options.email,
+    subject: options.subject,
+    text: options.message,
+  };
 
-    // 3) Send email
-    await transporter.sendMail(mailOpts, (error, info) => {
-        if (error) {
-            console.log(error);
-        } else {
-            console.log('Email sent: ' + info.response);
-        }
-    });
+  try {
+    const info = await transporter.sendMail(mailOpts);
+    /* console.log('Email sent: ' + info.response);
+    console.log('Email receiver: ' + options.email); */
+  } catch (error) {
+    console.log(error);
+  }
 };
 
-
-export default sendEmail; 
+export default sendEmail;
