ae992fe04460ddff5c9c06a8040ff62bfbbdca83
hussain <hussain87nyn@gmail.com>
2023-06-23 09:53:56 +0300

    Update brand and user validations
    - Modify utils/validations/BrandValidation.js
    - Modify utils/validations/UserValidation.js

--- utils/validations/BrandValidation.js
+++ utils/validations/BrandValidation.js
@@ -1,31 +1,39 @@
-import {check} from "express-validator";
+import { check } from "express-validator";
 import slugify from "slugify";
-import {validatorMiddleware} from "../../middlewares/ValidatorMiddleware.js";
+import { validatorMiddleware } from "../../middlewares/ValidatorMiddleware.js";
 
 export const ValidationbodyRulesForCreate = [
-    check("name")
-        .notEmpty()
-        .withMessage("brand name is required !")
-        .isString()
-        .withMessage("brand name must be string !")
-        .isLength({min: 3})
-        .withMessage("brand name is too short !")
-        .isLength({max: 32})
-        .withMessage("brand name is too long !")
-        .custom((val, {req}) => {
-            req.body.slug = slugify(val);
-            return true;
-        }),
-    validatorMiddleware,
+  check("name")
+    .notEmpty()
+    .withMessage("brand name is required!")
+    .isString()
+    .withMessage("brand name must be string!")
+    .isLength({ min: 3 })
+    .withMessage("brand name is too short!")
+    .isLength({ max: 32 })
+    .withMessage("brand name is too long!")
+    .custom((val, { req }) => {
+      req.body.slug = slugify(val);
+      return true;
+    }),
+  check("image").notEmpty().withMessage("Image is required!"),
+  validatorMiddleware,
 ];
 export const ValidationbodyRulesForUpdate = [
-    check("name")
-        .optional()
-        .isString()
-        .custom((val, {req}) => {
-            req.body.slug = slugify(val);
-            return true;
-        })
-    ,
-    validatorMiddleware,
+  check("name")
+    .notEmpty()
+    .withMessage("brand name is required!")
+    .isString()
+    .withMessage("brand name must be string!")
+    .isLength({ min: 3 })
+    .withMessage("brand name is too short!")
+    .isLength({ max: 32 })
+    .withMessage("brand name is too long!")
+    .custom((val, { req }) => {
+      req.body.slug = slugify(val);
+      return true;
+    }),
+  check("image").notEmpty().withMessage("Image is required!"),
+
+  validatorMiddleware,
 ];
